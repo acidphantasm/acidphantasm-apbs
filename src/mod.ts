8e3c4996f1349490ea53a24742562df548827f4b
acidphantasm <acidphantasm@acidphantasm.com>
2024-07-12 11:47:48 -0500

    Change structure & add in required imports

--- src/mod.ts
+++ src/mod.ts
@@ -2,27 +2,47 @@ import { DependencyContainer } from "tsyringe";
 
 // SPT
 import { IPostDBLoadMod } from "@spt/models/external/IPostDBLoadMod";
+import { IPreSptLoadMod } from "@spt/models/external/IPreSptLoadMod";
+import { StaticRouterModService } from "@spt/services/mod/staticRouter/StaticRouterModService";
+import { ItemHelper } from "@spt/helpers/ItemHelper";
 import { ILogger } from "@spt/models/spt/utils/ILogger";
 
 // Custom
 import { APBSLogger } from "./Utils/apbsLogger";
 import { Logging } from "./Enums/Logging";
+import { BotStaticRouterHook } from "./RouterHooks/BotStaticRouterHook";
 
-class APBS implements IPostDBLoadMod
+class APBS implements IPreSptLoadMod, IPostDBLoadMod
 {
-    private mod: string
     private logger: ILogger
+    private apbsLogger: APBSLogger
+    private staticRouter: StaticRouterModService
+    private itemHelper: ItemHelper
+
+    public preSptLoad(container: DependencyContainer): void 
+    {
+        this.logger = container.resolve<ILogger>("WinstonLogger");
+        this.staticRouter = container.resolve<StaticRouterModService>("StaticRouterModService");
+        this.itemHelper = container.resolve<ItemHelper>("ItemHelper")
+        this.apbsLogger = new APBSLogger();
+        this.apbsLogger.createLogFiles();
+
+        const botStaticRouterHook = new BotStaticRouterHook(this.staticRouter, this.itemHelper, this.apbsLogger, this.logger);
+
+        botStaticRouterHook.registerRouterHook();
+    }
 
     public postDBLoad(container: DependencyContainer): void
     {
         const start = performance.now()
-        const apbsLogger = new APBSLogger;
         this.logger = container.resolve<ILogger>("WinstonLogger");
+        this.apbsLogger = new APBSLogger();
+        
 
-        apbsLogger.createLogFiles();
+        // code is here reee
         
         const timeTaken = performance.now() - start;
-        apbsLogger.log(this.logger, `Time Taken: ${timeTaken}`, Logging.DEBUG);
+        this.apbsLogger.log(this.logger, Logging.DEBUG, `Time Taken: ${timeTaken}`);
     }
 }
 
