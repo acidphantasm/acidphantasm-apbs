3ed1932377d7cb103cb28277d15ce1f74b6d9dce
acidphantasm <acidphantasm@acidphantasm.com>
2024-07-15 18:41:25 -0500

    Update logging to include EVENT gen

--- src/Enums/Logging.ts
+++ src/Enums/Logging.ts
@@ -4,6 +4,7 @@ export enum Logging
     PMC = "pmc_generation",
     RAIDER = "raider_generation",
     BOSS = "boss_generation",
+    EVENT = "event_generation",
     WARN = "warnings",
     ERR = "errors",
     DEBUG = "debug"
