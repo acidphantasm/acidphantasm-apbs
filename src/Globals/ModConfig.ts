c191deb3d74cb66896eb3727a88887f09dfc7d1f
acidphantasm <acidphantasm@acidphantasm.com>
2024-07-28 02:38:21 -0500

    Add tier level deviation

--- src/Globals/ModConfig.ts
+++ src/Globals/ModConfig.ts
@@ -1,15 +1,62 @@
 import { VFS } from "@spt/utils/VFS";
-import { container } from "tsyringe";
+import { container, inject, injectable } from "tsyringe";
 import jsonc from "jsonc";
 import path from "path";
+import { TierInformation } from "./TierInformation";
+import { APBSLogger } from "../Utils/APBSLogger";
 
+@injectable()
 export class ModConfig
 {
-    private static vfs = container.resolve<VFS>("VFS");
-    public static config: Config = jsonc.parse(this.vfs.readFile(path.resolve(__dirname, "../../config/config.jsonc")));
+    public static config: Config;
+
+    constructor(
+        @inject("APBSLogger") protected apbsLogger: APBSLogger,
+        @inject("TierInformation") protected tierInformation: TierInformation,
+        @inject("VFS") protected vfs: VFS
+    )
+    {
+        ModConfig.config = jsonc.parse(this.vfs.readFile(path.resolve(__dirname, "../../config/config.jsonc")));
+    }
+
+    public setModConfiguration(): void
+    {
+        this.setTierLevelDeviation();
+    }
+
+    private setTierLevelDeviation(): void
+    {
+        this.tierInformation.tiers[0].botMinLevelVariance = ModConfig.config.tier1LevelDelta[0]
+        this.tierInformation.tiers[0].botMaxLevelVariance = ModConfig.config.tier1LevelDelta[1]
+
+        this.tierInformation.tiers[1].botMinLevelVariance = ModConfig.config.tier2LevelDelta[0]
+        this.tierInformation.tiers[1].botMaxLevelVariance = ModConfig.config.tier2LevelDelta[1]
+
+        this.tierInformation.tiers[2].botMinLevelVariance = ModConfig.config.tier3LevelDelta[0]
+        this.tierInformation.tiers[2].botMaxLevelVariance = ModConfig.config.tier3LevelDelta[1]
+
+        this.tierInformation.tiers[3].botMinLevelVariance = ModConfig.config.tier4LevelDelta[0]
+        this.tierInformation.tiers[3].botMaxLevelVariance = ModConfig.config.tier4LevelDelta[1]
+
+        this.tierInformation.tiers[4].botMinLevelVariance = ModConfig.config.tier5LevelDelta[0]
+        this.tierInformation.tiers[4].botMaxLevelVariance = ModConfig.config.tier5LevelDelta[1]
+
+        this.tierInformation.tiers[5].botMinLevelVariance = ModConfig.config.tier6LevelDelta[0]
+        this.tierInformation.tiers[5].botMaxLevelVariance = ModConfig.config.tier6LevelDelta[1]
+
+        this.tierInformation.tiers[6].botMinLevelVariance = ModConfig.config.tier7LevelDelta[0]
+        this.tierInformation.tiers[6].botMaxLevelVariance = ModConfig.config.tier7LevelDelta[1]
+    }
 }
 
 export interface Config
 {
-    enableModdedWeapons: boolean
+    enableModdedWeapons: boolean,
+    tier1LevelDelta: [ number, number ],
+    tier2LevelDelta: [ number, number ],
+    tier3LevelDelta: [ number, number ],
+    tier4LevelDelta: [ number, number ],
+    tier5LevelDelta: [ number, number ],
+    tier6LevelDelta: [ number, number ],
+    tier7LevelDelta: [ number, number ],
 }
