b9fa5ffdc9f7ab4ddf1eced3dbd116becabf830f
acidphantasm <acidphantasm@acidphantasm.com>
2024-09-16 00:27:33 -0500

    Adjust minLevelVariance (and config to match)

--- src/Globals/TierInformation.ts
+++ src/Globals/TierInformation.ts
@@ -51,49 +51,49 @@ export class TierInformation
             tier: 1,
             playerMinimumLevel: 1,
             playerMaximumLevel: 10,
-            botMinLevelVariance: 5,
+            botMinLevelVariance: 10,
             botMaxLevelVariance: 5
         },
         {
             tier: 2,
             playerMinimumLevel: 11,
             playerMaximumLevel: 20,
-            botMinLevelVariance: 7,
+            botMinLevelVariance: 15,
             botMaxLevelVariance: 7
         },
         {
             tier: 3,
             playerMinimumLevel: 21,
             playerMaximumLevel: 30,
-            botMinLevelVariance: 10,
+            botMinLevelVariance: 25,
             botMaxLevelVariance: 10
         },
         {
             tier: 4,
             playerMinimumLevel: 31,
             playerMaximumLevel: 40,
-            botMinLevelVariance: 15,
+            botMinLevelVariance: 35,
             botMaxLevelVariance: 15
         },
         {
             tier: 5,
             playerMinimumLevel: 41,
             playerMaximumLevel: 50,
-            botMinLevelVariance: 15,
+            botMinLevelVariance: 45,
             botMaxLevelVariance: 15
         },
         {
             tier: 6,
             playerMinimumLevel: 51,
             playerMaximumLevel: 60,
-            botMinLevelVariance: 20,
+            botMinLevelVariance: 55,
             botMaxLevelVariance: 20
         },
         {
             tier: 7,
             playerMinimumLevel: 61,
             playerMaximumLevel: 100,
-            botMinLevelVariance: 30,
+            botMinLevelVariance: 60,
             botMaxLevelVariance: 20
         }
     ]
