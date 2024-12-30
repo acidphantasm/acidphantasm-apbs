3ba0a3a6095c03bc222bb7846cb7d27f30432dde
acidphantasm <acidphantasm@acidphantasm.com>
2024-12-29 23:53:48 -0600

    config update

--- src/Globals/ModConfig.ts
+++ src/Globals/ModConfig.ts
@@ -63,6 +63,7 @@ export interface Config
     slideChance: number,
     enablePerWeaponTypeAttachmentChances: boolean,
     forceStock: boolean,
+    stockButtpadChance: number,
     forceDustCover: boolean,
     forceScopeSlot: boolean,
     forceMuzzle: boolean,
