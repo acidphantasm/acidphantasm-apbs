2828c17e779b8805cacced05bf9671ac0760b866
acidphantasm <acidphantasm@acidphantasm.com>
2025-01-14 00:36:36 -0600

    If resource value rolls below 1 then set to 1

--- src/ClassExtensions/APBSBotGeneratorHelper.ts
+++ src/ClassExtensions/APBSBotGeneratorHelper.ts
@@ -17,6 +17,7 @@ import { ITemplateItem } from "@spt/models/eft/common/tables/ITemplateItem";
 import { IGetRaidConfigurationRequestData } from "@spt/models/eft/match/IGetRaidConfigurationRequestData";
 import { BaseClasses } from "@spt/models/enums/BaseClasses";
 import { LocalisationService } from "@spt/services/LocalisationService";
+import { IRandomisedResourceValues } from "@spt/models/spt/config/IBotConfig";
 
 /** Handle profile related client events */
 @injectable()
@@ -48,118 +49,22 @@ export class APBSBotGeneratorHelper extends BotGeneratorHelper
             configServer)
     }
 
-    public override generateExtraPropertiesForItem(itemTemplate: ITemplateItem, botRole?: string): { upd?: IUpd } 
+    protected override getRandomizedResourceValue(maxResource: number, randomizationValues: IRandomisedResourceValues): number
     {
-        // Get raid settings, if no raid, default to day
-        const raidSettings = this.applicationContext
-            .getLatestValue(ContextVariableType.RAID_CONFIGURATION)
-            ?.getValue<IGetRaidConfigurationRequestData>();
-        const raidIsNight = raidSettings?.timeVariant === "PAST";
-
-        const itemProperties: IUpd = {};
-
-        if (itemTemplate._props.MaxDurability) 
+        if (!randomizationValues) 
         {
-            if (itemTemplate._props.weapClass) 
-            {
-                // Is weapon
-                itemProperties.Repairable = this.generateWeaponRepairableProperties(itemTemplate, botRole);
-            }
-            else if (itemTemplate._props.armorClass) 
-            {
-                // Is armor
-                itemProperties.Repairable = this.generateArmorRepairableProperties(itemTemplate, botRole);
-            }
+            return maxResource;
         }
 
-        if (itemTemplate._props.HasHinge) 
+        if (this.randomUtil.getChance100(randomizationValues.chanceMaxResourcePercent)) 
         {
-            itemProperties.Togglable = { On: true };
+            return maxResource;
         }
 
-        if (itemTemplate._props.Foldable) 
-        {
-            itemProperties.Foldable = { Folded: false };
-        }
-
-        if (itemTemplate._props.weapFireType?.length) 
-        {
-            if (itemTemplate._props.weapFireType.includes("fullauto")) 
-            {
-                itemProperties.FireMode = { FireMode: "fullauto" };
-            }
-            else 
-            {
-                itemProperties.FireMode = { FireMode: this.randomUtil.getArrayValue(itemTemplate._props.weapFireType) };
-            }
-        }
-
-        if (itemTemplate._props.MaxHpResource) 
-        {
-            itemProperties.MedKit = {
-                HpResource: this.getRandomizedResourceValue(
-                    itemTemplate._props.MaxHpResource,
-                    this.botConfig.lootItemResourceRandomization[botRole]?.meds
-                )
-            };
-        }
-
-        if (itemTemplate._props.MaxResource > 1 && itemTemplate._props.foodUseTime)
-        {
-            itemProperties.FoodDrink = {
-                HpPercent: this.getRandomizedResourceValue(
-                    itemTemplate._props.MaxResource,
-                    this.botConfig.lootItemResourceRandomization[botRole]?.food
-                )
-            };
-        }
-
-        if (itemTemplate._parent === BaseClasses.FLASHLIGHT) 
-        {
-            // Get chance from botconfig for bot type
-            const lightLaserActiveChance = raidIsNight
-                ? this.getBotEquipmentSettingFromConfig(botRole, "lightIsActiveNightChancePercent", 50)
-                : this.getBotEquipmentSettingFromConfig(botRole, "lightIsActiveDayChancePercent", 25);
-            itemProperties.Light = {
-                IsActive: this.randomUtil.getChance100(lightLaserActiveChance),
-                SelectedMode: 0
-            };
-        }
-        else if (itemTemplate._parent === BaseClasses.TACTICAL_COMBO) 
-        {
-            // Get chance from botconfig for bot type, use 50% if no value found
-            const lightLaserActiveChance = this.getBotEquipmentSettingFromConfig(
-                botRole,
-                "laserIsActiveChancePercent",
-                50
-            );
-            itemProperties.Light = {
-                IsActive: this.randomUtil.getChance100(lightLaserActiveChance),
-                SelectedMode: 0
-            };
-        }
-
-        if (itemTemplate._parent === BaseClasses.NIGHTVISION) 
-        {
-            // Get chance from botconfig for bot type
-            const nvgActiveChance = raidIsNight
-                ? this.getBotEquipmentSettingFromConfig(botRole, "nvgIsActiveChanceNightPercent", 90)
-                : this.getBotEquipmentSettingFromConfig(botRole, "nvgIsActiveChanceDayPercent", 15);
-            itemProperties.Togglable = { On: this.randomUtil.getChance100(nvgActiveChance) };
-        }
-
-        // Togglable face shield
-        if (itemTemplate._props.HasHinge && itemTemplate._props.FaceShieldComponent) 
-        {
-            // Get chance from botconfig for bot type, use 75% if no value found
-            const faceShieldActiveChance = this.getBotEquipmentSettingFromConfig(
-                botRole,
-                "faceShieldIsActiveChancePercent",
-                75
-            );
-            itemProperties.Togglable = { On: this.randomUtil.getChance100(faceShieldActiveChance) };
-        }
-
-        return Object.keys(itemProperties).length ? { upd: itemProperties } : {};
+        const newResourceValue = this.randomUtil.getInt(
+            this.randomUtil.getPercentOfValue(randomizationValues.resourcePercent, maxResource, 0),
+            maxResource
+        )
+        return newResourceValue ? newResourceValue : 1;
     }
 }
