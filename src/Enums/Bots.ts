c0ca86726142736f627a78e735b3c56bb787fa6e
acidphantasm <acidphantasm@acidphantasm.com>
2025-01-30 01:00:52 -0600

    Fold all bot types into an Enum for easier updating, validating, and expansion

---
+++ src/Enums/Bots.ts
@@ -0,0 +1,86 @@
+export enum Boss
+    {
+    BOSSBOAR = "bossboar",
+    BOSSBULLY = "bossbully",
+    BOSSGLUHAR = "bossgluhar",
+    BOSSKILLA = "bosskilla",
+    BOSSKOJANIY = "bosskojaniy",
+    BOSSKOLONTAY = "bosskolontay",
+    BOSSPARTISAN = "bosspartisan",
+    BOSSSANITAR = "bosssanitar",
+    BOSSTAGILLA = "bosstagilla",
+    BOSSZRYACHIY = "bosszryachiy",
+    BOSSKNIGHT = "bossknight",
+    FOLLOWERBIGPIPE = "followerbigpipe",
+    FOLLOWERBIRDEYE = "followerbirdeye"
+}
+
+export enum Follower
+    {
+    BOSSBOARSNIPER = "bossboarsniper",
+    FOLLOWERBOAR = "followerboar",
+    FOLLOWERBOARCLOSE1 = "followerboarclose1",
+    FOLLOWERBOARCLOSE2 = "followerboarclose2",
+    FOLLOWERBULLY = "followerbully",
+    FOLLOWERGLUHARASSAULT = "followergluharassault",
+    FOLLOWERGLUHARSCOUT = "followergluharscout",
+    FOLLOWERGLUHARSNIPE = "followergluharsnipe",
+    FOLLOWERKOJANIY = "followerkojaniy",
+    FOLLOWERKOLONTAYASSAULT = "followerkolontayassault",
+    FOLLOWERKOLONTAYSECURITY = "followerkolontaysecurity",
+    FOLLOWERSANITAR = "followersanitar",
+    FOLLOWERTAGILLA = "followertagilla",
+    FOLLOWERZRYACHIY = "followerzryachiy"
+}
+
+export enum Special
+    {
+    ARENAFIGHTER = "arenafighter",
+    ARENAFIGHTEREVENT = "arenafighterevent",
+    EXUSEC = "exusec",
+    PMCBOT = "pmcbot"
+}
+
+export enum PMC
+    {
+    PMCUSEC = "pmcusec",
+    PMCBEAR = "pmcbear",
+    USEC = "usec",
+    BEAR = "bear"
+}
+
+export enum Scav
+    {
+    ASSAULT = "assault",
+    ASSAULTGROUP = "assaultgroup",
+    CRAZYASSAULT = "crazyassault",
+    CRAZYASSAULTEVENT = "crazyassaultevent",
+    CURSEDASSAULT = "cursedassault",
+    MARKSMAN = "marksman"
+}
+
+export enum Event 
+    {
+    GIFTER = "gifter",
+    INFECTEDASSAULT = "infectedassault",
+    INFECTEDCIVIL = "infectedcivil",
+    INFECTEDLABORANT = "infectedlaborant",
+    INFECTEDPMC = "infectedpmc",
+    INFECTEDTAGILLA = "infectedtagilla",
+    PEACEFULLZRYACHIYEVENT = "peacefullzryachiyevent",
+    PEACEMAKER = "peacemaker",
+    RAVANGEZRYACHIYEVENT = "ravangezryachiyevent",
+    SECTACTPRIESTEVENT = "sectantpriestevent",
+    SECTANTONI = "sectantoni",
+    SECTANTPREDVESTNIK = "sectantpredvestnik",
+    SECTANTPRIZRAK = "sectantprizrak",
+    SHOOTERBTR = "shooterbtr",
+    SKIER = "skier",
+    SPIRITSPRING = "spiritspring",
+    SPIRITWINTER = "spiritwinter",
+    TEST = "test",
+    FOLLOWERTEST = "followertest",
+    BOSSTEST = "bosstest",
+    BOSSLEGION = "bosslegion",
+    BOSSPUNISHER = "bosspunisher"
+}
